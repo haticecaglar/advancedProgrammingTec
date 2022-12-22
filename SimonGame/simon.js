0b8c35b2a95925b1223d1d8b9a4af6bff772f593
haticecaglar <hatice.caglar@stu.fsm.edu.tr>
2022-12-22 13:00:11 +0300

    Update

---
+++ SimonGame/simon.js
@@ -0,0 +1,59 @@
+const topLeft = document.querySelector('.top-left-panel');
+const topRight = document.querySelector('.top-right-panel');
+const bottomLeft = document.querySelector('.bottom-left-panel');
+const bottomRight = document.querySelector('.bottom-right-panel');
+
+const getRandomPanel = () => {
+    const panels = [
+        topLeft,
+        topRight,
+        bottomLeft,
+        bottomRight
+    ];
+    return panels[parseInt(Math.random() * panels.length)];
+
+};
+
+const sequences = [getRandomPanel()];
+let sequencesToGuess = [...sequences];
+
+const flash = (panel) => {
+    return new Promise(resolve => {
+        panel.className += 'active';
+        setTimeout(() => {
+            panel.className = panel.className.replace('active', '');
+            setTimeout(() => {
+                resolve();
+            }, 250);
+
+        }, 1000)
+    });
+};
+let canClick = false;
+
+const panelClicked = panelClicked => {
+    if (!canClick) return;
+    const expectedPanel = sequencesToGuess.shift();
+    if (expectedPanel === panelClicked) {
+        if (sequencesToGuess.length === 0) {
+            //Start new round
+            sequences.push(getRandomPanel());
+            sequencesToGuess=[...sequences];
+            startFlashing();
+        }
+    }else{
+        //end game
+        alert('game over');
+    }
+};
+
+const startFlashing= async () => {
+    canClick = false;
+    for (const panel of sequences) {
+        await flash(panel);
+    }
+    canClick = true;
+
+}
+
+startFlashing();
