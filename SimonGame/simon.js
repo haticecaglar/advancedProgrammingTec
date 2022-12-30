543f17446f24bf794982b1f07dc6e6106107ab41
haticecaglar <hatice.caglar@stu.fsm.edu.tr>
2022-12-30 13:57:28 +0300

    update

--- SimonGame/simon.js
+++ SimonGame/simon.js
@@ -7,17 +7,15 @@ const gameOverScreen = document.getElementById('game-over-screen');
 const restartButton = document.getElementById('restart-button');
 const hearts = document.getElementById("hearts");
 let score = 0;
-
+let live = 3;
+let heart_id=3;
 function showGameOver() {
     document.getElementById("game-over-screen").style.display = "block";
-  }
+    restartButton.addEventListener('click', restartGame);
+}
   
-  function hideGameOver() {
+function hideGameOver() {
     document.getElementById("game-over-screen").style.display = "none";
-  }
-function restartGame() {
-    // Reset game state here
-    hideGameOver();
 }
 
 const getRandomPanel = () => {
@@ -31,8 +29,23 @@ const getRandomPanel = () => {
 
 };
 
-const sequences = [getRandomPanel()];
+let sequences = [getRandomPanel()];
 let sequencesToGuess = [...sequences];
+function restartGame() {
+    // Reset game state here
+    hideGameOver();
+    document.getElementById('1').style.visibility="visible";
+    document.getElementById('2').style.visibility="visible";
+    document.getElementById('3').style.visibility="visible";
+    score=0;
+    sequences=[];
+    live=3;
+    sequencesToGuess = [...sequences];
+    skor_html.innerHTML = "Skor:".concat(score);
+    sequences = [getRandomPanel()];
+    sequencesToGuess = [...sequences];
+
+}
 
 const flash = (panel) => {
     return new Promise(resolve => {
@@ -62,14 +75,20 @@ const panelClicked = panelClicked => {
             startFlashing();
 
         }
-    } else {
-        //end game
-        // alert('game over');
-        showGameOver();
-        restartButton.addEventListener('click', restartGame);
-        
+    } else{
+        var child = document.getElementById(live); 
+        child.style.visibility="hidden";
+        live--;      
+        if(live>0){
+            sequences.push(getRandomPanel());
+            sequencesToGuess = [...sequences];
+            startFlashing();
+        }else{
+            showGameOver();
+        }
      
-
+        //showGameOver();
+        
     }
     
     
